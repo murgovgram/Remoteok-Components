2f151f445bc81572bab6dffda474f5e7ef22888e
Filip Murgovski <murgovski@gmail.com>
2021-06-02 23:08:01 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./public/**/*.html'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+      }
+      
+    },
+  variants: {
+    extend: {},
+    display: ['responsive', 'group-hover', 'group-focus'],
+  },
+  plugins: []
+}
